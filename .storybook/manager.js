1027aeb391e9f69e5d296e9f39db26d89091a7ec
Dennis Shem Limo <159112148+Shemshemy@users.noreply.github.com>
2025-07-20 21:17:57 +0300

    Naming

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,7 +5,7 @@ addons.setConfig({
   theme: {
     ...themes.dark,
     brandImage: './icon.svg',
-    brandTitle: 'Hamish Williams Components',
+    brandTitle: 'Dennis Shem Limo Components',
     brandUrl: 'https://hamishw.com',
   },
 });
