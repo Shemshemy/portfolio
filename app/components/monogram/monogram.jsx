fb8d62f35f37b0d14b65768c7b7379b65884c8ca
Dennis Shem Limo <159112148+Shemshemy@users.noreply.github.com>
2025-07-21 21:36:33 +0300

    Changed logo to D

--- app/components/monogram/monogram.jsx
+++ app/components/monogram/monogram.jsx
@@ -17,9 +17,13 @@ export const Monogram = forwardRef(({ highlight, className, ...props }, ref) => 
       {...props}
     >
       <defs>
-        <clipPath id={clipId}>
-          <path d="M0 0h6.5a6 6 0 0 1 5.2 3.1L19.4 17l4-9L19 0h6.5a6 6 0 0 1 5.2 3.1L39.5 19 35 29 24.5 10 16 29 0 0Zm46.7 2.8A2 2 0 0 0 45 0h-7l5.5 10 3.2-7.2Z" />
-        </clipPath>
+<clipPath id={clipId}>
+  {/* Extra-small D shape */}
+  <path d="M4 4 H12 
+           A6 8 0 0 1 12 28 
+           H4 
+           Z" />
+</clipPath>
       </defs>
       <rect clipPath={`url(#${clipId})`} width="100%" height="100%" />
       {highlight && (
